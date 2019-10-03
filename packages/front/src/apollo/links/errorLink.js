4f1fb920bcf9fd816cf706ea22cba0fbfaede79d
stw <temm1210@naver.com>
2019-10-03 14:09:16 +0900

    add networkError parameter

--- packages/front/src/apollo/links/errorLink.js
+++ packages/front/src/apollo/links/errorLink.js
@@ -5,44 +5,49 @@ import httpLink from "./httpLink";
 import { setContextHeader } from "../helpers";
 import { getTokenOperation } from "../constant";
 
-export default onError(({ graphQLErrors, operation, forward }) => {
-  const isWindow = typeof sessionStorage !== "undefined";
-  console.log("graphQLErrors:", graphQLErrors);
+export default onError(
+  // eslint-disable-next-line consistent-return
+  ({ graphQLErrors, operation, networkError, forward }) => {
+    const isWindow = typeof sessionStorage !== "undefined";
+    if (networkError) console.log(`[Network error]: ${networkError}`);
 
-  switch (graphQLErrors[0].extensions.code) {
-    case "UNAUTHENTICATED": {
-      console.log("JWT EXPIRED");
-      return new Observable(observer => {
-        execute(httpLink, getTokenOperation.operation).subscribe({
-          next: ({ data }) => {
-            const { access_token, refresh_token } = data[
-              getTokenOperation.name
-            ];
+    if (graphQLErrors) {
+      switch (graphQLErrors[0].extensions.code) {
+        case "UNAUTHENTICATED": {
+          console.log("JWT EXPIRED");
+          return new Observable(observer => {
+            execute(httpLink, getTokenOperation.operation).subscribe({
+              next: ({ data }) => {
+                const { access_token, refresh_token } = data[
+                  getTokenOperation.name
+                ];
 
-            if (isWindow) {
-              sessionStorage.setItem("access_token", access_token);
-              sessionStorage.setItem("refresh_token", refresh_token);
+                if (isWindow) {
+                  sessionStorage.setItem("access_token", access_token);
+                  sessionStorage.setItem("refresh_token", refresh_token);
 
-              setContextHeader(operation, {
-                Authorization: `Bearer ${sessionStorage.getItem(
-                  "access_token",
-                )}`,
-              });
-            }
+                  setContextHeader(operation, {
+                    Authorization: `Bearer ${sessionStorage.getItem(
+                      "access_token",
+                    )}`,
+                  });
+                }
 
-            return forward(operation).subscribe(observer);
-          },
-          error: error => console.log(`${error}`),
-        });
-      });
+                return forward(operation).subscribe(observer);
+              },
+              error: error => console.log(`${error}`),
+            });
+          });
+        }
+
+        case "FORBIDDEN": {
+          if (isWindow) return window.alert("YOU HAVE TO LOGIN");
+          return null;
+        }
+        default: {
+          return forward(operation);
+        }
+      }
     }
-
-    case "FORBIDDEN": {
-      if (isWindow) return window.alert("YOU HAVE TO LOGIN");
-      return null;
-    }
-    default: {
-      return forward(operation);
-    }
-  }
-});
+  },
+);
