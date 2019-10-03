deddb42b11c4ae516e9bb0f9aae5aa354636c604
stw <temm1210@naver.com>
2019-10-03 14:10:31 +0900

    update content

--- packages/front/src/components/pages/Template/Footer/FooterList/FooterCopyRight/FooterCopyRight.js
+++ packages/front/src/components/pages/Template/Footer/FooterList/FooterCopyRight/FooterCopyRight.js
@@ -4,38 +4,7 @@ import FooterList from "../FooterList";
 const footerCopyRight = [
   {
     id: 1,
-    text: "Copyright © 2019",
-    size: "verySmall",
-  },
-  {
-    id: 2,
-    text:
-      "송택우(웹,모바일 - React, React-Native, MySql, Docker, Express, Kafka)",
-    size: "verySmall",
-  },
-  {
-    id: 3,
-    text: "박현준(DB,파이프라인 - MySql, Redis,Kafka)",
-    size: "verySmall",
-  },
-  {
-    id: 4,
-    text: "이아연(UI/UX디자인 - React, UI/UX, CSS, HTML)",
-    size: "verySmall",
-  },
-  {
-    id: 5,
-    text: "김민주(UI/UX디자인 - React, React-Native, UI/UX, CSS, HTML,Sketch)",
-    size: "verySmall",
-  },
-  {
-    id: 6,
-    text: "박희진(기획 - 기획, 설계, Sketch, UI/UX)",
-    size: "verySmall",
-  },
-  {
-    id: 7,
-    text: "이성근(튜닝,운영 - Express튜닝, MySql튜닝,Docker, Kubernetes,Kafka)",
+    text: "Copyright © 2019 송택우",
     size: "verySmall",
   },
 ];
