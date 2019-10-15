289dc9ef86f92fca9d64607fabf5611a1caed5ca
stw <temm1210@naver.com>
2019-10-15 22:29:38 +0900

    change apolloserver class to factory

--- packages/back/src/apollo/server.js
+++ packages/back/src/apollo/server.js
@@ -10,51 +10,55 @@ import { userFields } from "../db/schemas";
 const typeDefs = fileLoader(path.join(__dirname, "/**/*Graphql*.js"));
 const resolvers = fileLoader(path.join(__dirname, "/**/*Resolvers.js"));
 
-const userAPI = new UserAPI(pool);
-const boardAPI = new BoardAPI(pool);
-
 export const schema = makeExecutableSchema({
   typeDefs: mergeTypes(typeDefs),
   resolvers: mergeResolvers(resolvers),
 });
 
-export default new ApolloServer({
-  schema,
-  dataSources: () => ({
-    userAPI,
-    boardAPI,
-  }),
-  // query를 apollo-link의 execute로 실행시 operation값들은 req.body에 담겨서옴
-  context: async ({ req }) => {
-    const {
-      headers: { authorization },
-      body: { extensions },
-    } = req;
+export default () => {
+  const userAPI = new UserAPI(pool);
+  const boardAPI = new BoardAPI(pool);
 
-    try {
-      // 클라이언트에서 extensions를 허용해주면 해당값이 담겨줘서 넘어옴
-      const grant_type = extensions && extensions.grant_type;
-      const refresh_token = extensions && extensions.refresh_token;
-      const jwtToken =
-        grant_type && grant_type === "refresh_token"
-          ? refresh_token.replace("Bearer ", "").trim()
-          : authorization.replace("Bearer ", "").trim();
+  // boardAPI.testBoard();
 
-      if (jwtToken === "null" || jwtToken === "undefined") {
-        return { user: null };
+  return new ApolloServer({
+    schema,
+    dataSources: () => ({
+      userAPI,
+      boardAPI,
+    }),
+    // query를 apollo-link의 execute로 실행시 operation값들은 req.body에 담겨서옴
+    context: async ({ req }) => {
+      const {
+        headers: { authorization },
+        body: { extensions },
+      } = req;
+
+      try {
+        // 클라이언트에서 extensions를 허용해주면 해당값이 담겨줘서 넘어옴
+        const grant_type = extensions && extensions.grant_type;
+        const refresh_token = extensions && extensions.refresh_token;
+        const jwtToken =
+          grant_type && grant_type === "refresh_token"
+            ? refresh_token.replace("Bearer ", "").trim()
+            : authorization.replace("Bearer ", "").trim();
+
+        if (jwtToken === "null" || jwtToken === "undefined") {
+          return { user: null };
+        }
+
+        const jwtDecoded = jwt.verify(jwtToken, process.env.jwt_secret);
+        const user = await userAPI.getUser({
+          [userFields.EMAIL]: jwtDecoded.data,
+        });
+
+        return { user };
+      } catch (error) {
+        if (error.name === "TokenExpiredError") {
+          console.log("EXPIRED!!!!!");
+          throw new AuthenticationError("Token expired");
+        }
       }
-
-      const jwtDecoded = jwt.verify(jwtToken, process.env.jwt_secret);
-      const user = await userAPI.getUser({
-        [userFields.EMAIL]: jwtDecoded.data,
-      });
-
-      return { user };
-    } catch (error) {
-      if (error.name === "TokenExpiredError") {
-        console.log("EXPIRED!!!!!");
-        throw new AuthenticationError("Token expired");
-      }
-    }
-  },
-});
+    },
+  });
+};
