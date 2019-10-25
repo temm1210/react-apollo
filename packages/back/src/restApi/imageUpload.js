48b1045b860b80cd657c7d9be4f4609228ad2227
stw <temm1210@naver.com>
2019-10-25 09:05:45 +0900

    move try to top

--- packages/back/src/restApi/imageUpload.js
+++ packages/back/src/restApi/imageUpload.js
@@ -22,13 +22,13 @@ const upload = multer({ storage });
 const router = express.Router();
 
 router.post("/", upload.single("editorImage"), async (req, res) => {
-  // image의 메타데이터중 width값을 가져옴
-  const metaReader = sharp(req.file.path);
-  const { width } = await metaReader.metadata();
-
-  // resize된 이미지앞에 resize라는 이름을 붙여줌
-  const resizeFileName = `resize_${req.file.filename}`;
   try {
+    // image의 메타데이터중 width값을 가져옴
+    const metaReader = sharp(req.file.path);
+    const { width } = await metaReader.metadata();
+
+    // resize된 이미지앞에 resize라는 이름을 붙여줌
+    const resizeFileName = `resize_${req.file.filename}`;
     // 이미지의 width값이 850보다 크면 sharp를 통해 850으로
     // resizing후 다시 저장. 이후 resizing된 image를
     // client로 보내줌
